9effc67cc9d9ee67230c8a34c607d6c3bcdde0d0
Dennis Muvaa <dmuvaa70@gmail.com>
2023-03-23 04:09:56 -0700

    File for Stack Operations

--- stack_operations.c
+++ stack_operations.c
@@ -1,4 +1,10 @@
-#include "interpreter.h"
+#include "monty.h"
+
+/**
+ * push - Push a value onto the stack
+ * @stack: Double pointer to the stack
+ * @value: Value to push onto the stack
+ */
 
 void push(stack_node_t **stack, int value)
 {
@@ -14,6 +20,11 @@ void push(stack_node_t **stack, int value)
 	*stack = new_node;
 }
 
+/**
+ * pall - Print all values on the stack
+ * @stack: Pointer to the stack
+ */
+
 void pall(stack_node_t *stack)
 {
 	stack_node_t *current = stack;
@@ -24,6 +35,11 @@ void pall(stack_node_t *stack)
 		current = current->next;
 	}
 }
+
+/**
+ * free_stack - Free the memory allocated for the stack
+ * @stack: Pointer to the stack
+ */
 
 void free_stack(stack_node_t *stack)
 {
