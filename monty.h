35aa3fa01a4454e131cb5831a63ba786d2431ba9
Dennis Muvaa <dmuvaa70@gmail.com>
2023-03-23 04:36:25 -0700

    Updated Main header file

--- monty.h
+++ monty.h
@@ -35,4 +35,12 @@ typedef struct instruction_s
 void push(stack_t **stack, unsigned int line_number);
 void pall(stack_t **stack, unsigned int line_number);
 
+void process_file(FILE *file, stack_node_t **stack);
+void process_line(char *line, unsigned int line_number, stack_node_t **stack);
+void execute_instruction(const char *opcode, const char *arg, unsigned int line_number, stack_node_t **stack);
+int is_valid_number(const char *arg);
+void push(stack_node_t **stack, int value);
+void pall(stack_node_t *stack);
+void free_stack(stack_node_t *stack);
+
 #endif
