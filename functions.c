08615ad4dd2910711e534c144523721ec6866b2d
brian-ambani <brainambani1@gmail.com>
2023-03-23 01:56:06 -0700

    updated function push and pall

---
+++ functions.c
@@ -0,0 +1,61 @@
+#include "monty.h"
+
+/**
+ * new_Node - creates a new node
+ * @n: is a value
+ * Return: new node
+ */
+stack_t *new_Node(int n)
+{
+	stack_t *new = NULL;
+
+	new = malloc(sizeof(stack_t));
+	if (new == NULL)
+	{
+		dprintf(STDERR_FILENO, "Error: malloc failed\n");
+		exit(EXIT_FAILURE);
+	}
+	new->n = n;
+	new->next = NULL;
+	new->prev = NULL;
+
+	return (new);
+}
+
+/**
+ * push - push item to the stack
+ * @stack: is a parameter
+ * @line_number: is value
+ */
+void push(stack_t **stack, unsigned int line_number)
+{
+	stack_t *new = NULL;
+	(void)line_number;
+
+	new = new_Node(value);
+
+	new->next = *stack;
+	if (*stack != NULL)
+		(*stack)->prev = new;
+	*stack = new;
+}
+
+/**
+ * pall - print elements stack
+ * @stack: is a parameter
+ * @n: is value
+ * Return: nothing
+ */
+void pall(stack_t **stack, unsigned int n)
+{
+	stack_t *current = NULL;
+	(void)n;
+
+	current = *stack;
+
+	while (current != NULL)
+	{
+		dprintf(STDOUT_FILENO, "%d\n", current->n);
+		current = current->next;
+	}
+}
